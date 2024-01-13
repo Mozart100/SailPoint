09f6bc556bd235d5968776a98f2045f0acfc5d09
Arkovean <anatoliy.arkove@gmail.com>
2024-01-13 06:36:53 +0200

    working manual version

--- SailPoint.Client/sailpoint/src/app/auto-complete-textbox/auto-complete-textbox.component.ts
+++ SailPoint.Client/sailpoint/src/app/auto-complete-textbox/auto-complete-textbox.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-auto-complete-textbox',
   templateUrl: './auto-complete-textbox.component.html',
-  styleUrls: ['./auto-complete-textbox.component.scss']
+  styleUrls: ['./auto-complete-textbox.component.scss'],
 })
-export class AutoCompleteTextboxComponent {
-  colorArray = ['red','blue','green'];
+export class AutoCompleteTextboxComponent implements OnInit {
+  colorArray = ['red', 'blue', 'green'];
+  filterOptions!: Observable<string[]>;
+  formControl: FormControl = new FormControl('');
+
+  ngOnInit(): void {
+    this.filterOptions = this.formControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._FILTER(value || ''))
+    );
+  }
+
+  private _FILTER(target: string): string[] {
+    const searchValue = target.toLocaleLowerCase();
+    return this.colorArray.filter((x) =>
+      x.toLocaleLowerCase().includes(searchValue)
+    );
+  }
 }
