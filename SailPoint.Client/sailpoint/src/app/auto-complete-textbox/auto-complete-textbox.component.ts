d5398d35f7398662cb5486a1209ce23c9ac774f9
Arkovean <anatoliy.arkove@gmail.com>
2024-01-15 18:28:00 +0200

    Working E2E

--- SailPoint.Client/sailpoint/src/app/auto-complete-textbox/auto-complete-textbox.component.ts
+++ SailPoint.Client/sailpoint/src/app/auto-complete-textbox/auto-complete-textbox.component.ts
@@ -1,39 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable, catchError, debounceTime, map, of, startWith, switchMap } from 'rxjs';
-import { CityLocatorService } from '../services/city.locator.service';
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import {FormControl} from '@angular/forms'
+import {Observable, catchError, debounceTime, map, of, startWith, switchMap, Subject, takeUntil} from 'rxjs'
+import {CityLocatorService} from '../services/city.locator.service'
 
 @Component({
   selector: 'app-auto-complete-textbox',
   templateUrl: './auto-complete-textbox.component.html',
   styleUrls: ['./auto-complete-textbox.component.scss'],
 })
-export class AutoCompleteTextboxComponent implements OnInit {
-  cities$!:Observable<string[]>;
-  formControl: FormControl = new FormControl('');
+export class AutoCompleteTextboxComponent implements OnInit, OnDestroy {
 
-  constructor(private cityservice: CityLocatorService) {}
+  formControl: FormControl = new FormControl('')
+  cities: string[] = []
+
+  private $destroy = new Subject()
+
+  constructor(private cityservice: CityLocatorService) {
+  }
 
   lookup(value: string): Observable<string[]> {
-    return this.cityservice.search(value.toLowerCase(),2).pipe(
+    return this.cityservice.search(value.toLowerCase(), 2).pipe(
       map(results => results.cities),
       catchError(_ => {
-        return of([]);
+        return of([])
       })
-    );
+    )
   }
 
   ngOnInit() {
-    this.cities$ = this.formControl.valueChanges.pipe(
+    this.formControl.valueChanges.pipe(
+      takeUntil(this.$destroy),
       startWith(''),
       debounceTime(300),
-      switchMap(value => {
-        if (value !== '') {
-          return this.lookup(value);
-        } else {
-          return of([]);
-        }
-      })
-    );
+      switchMap(value => value !== '' ? this.lookup(value) : of([]))
+    ).subscribe(cities => this.cities = cities)
   }
+
+  select(city: string) {
+    this.formControl.setValue(city, {emitEvent: false})
+    this.cities = []
+  }
+
+  ngOnDestroy(): void {
+    this.$destroy.next(null)
+  }
+
 }
