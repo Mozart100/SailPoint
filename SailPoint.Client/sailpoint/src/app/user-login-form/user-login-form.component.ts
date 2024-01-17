99ab0c49488ae5fb21ea4c67bc2efb195ff00acf
Arkovean <anatoliy.arkove@gmail.com>
2024-01-17 03:06:58 +0200

    error codes

--- SailPoint.Client/sailpoint/src/app/user-login-form/user-login-form.component.ts
+++ SailPoint.Client/sailpoint/src/app/user-login-form/user-login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login-form',
@@ -8,11 +8,27 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class UserLoginFormComponent implements OnInit {
   signupForm!: FormGroup;
+  userNamesInvalid =["xxx","kkk"];
   
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'userName':new FormControl(null),
-      'email':new FormControl(null),
+      'userName':new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
+      'email':new FormControl(null,[Validators.required,Validators.email]),
     });
+  }
+
+  onSubmit()
+  {
+    console.log(this.signupForm);
+  }
+
+  forbiddenNames(control:FormControl): {[key:string]:boolean} | null
+  {
+    if(this.userNamesInvalid.indexOf(control.value)>=0)
+    {
+        return {"xxx":true};
+    }
+
+    return null;
   }
 }
