2f96d43a30c48b5358dd4b4d6cf6f4aa9b59b2d1
Arkovean <anatoliy.arkove@gmail.com>
2024-02-16 20:24:38 +0200

    everything is working

--- SailPoint.Client/sailpoint/src/app/display-all-cities/display-all-cities.component.ts
+++ SailPoint.Client/sailpoint/src/app/display-all-cities/display-all-cities.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CityLocatorService } from '../services/city.locator.service';
-import { map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-display-all-cities',
@@ -8,19 +8,14 @@ import { map } from 'rxjs';
   styleUrls: ['./display-all-cities.component.scss'],
 })
 export class DisplayAllCitiesComponent implements OnInit {
-  cities: string[] = [];
-  cities$ = this.cityLocatorService
-    .getAllCities()
-    .pipe(map((response) => response.cities));
-
+  cities$:Observable<string[]> = new Observable<string[]>();
+  
   constructor(private cityLocatorService: CityLocatorService) {}
 
   ngOnInit(): void {
-    // this.cityLocatorService.getAllCities().pipe(
-    //   map((response) => {
-    //     this.cities = response.cities;
-    //   })
-    // );
+    this.cities$ = this.cityLocatorService
+    .getAllCities()
+    .pipe(map((response) => response.cities));
   }
 
   getColor(city: string): string {
--- SailPoint.Client/sailpoint/src/app/user-login-form/user-login-form.component.ts
+++ SailPoint.Client/sailpoint/src/app/user-login-form/user-login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-login-form',
@@ -8,27 +9,53 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class UserLoginFormComponent implements OnInit {
   signupForm!: FormGroup;
-  userNamesInvalid =["xxx","kkk"];
-  
+  userNamesInvalid = ['xxx', 'kkk'];
+
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'userName':new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
-      'email':new FormControl(null,[Validators.required,Validators.email]),
+      userName: new FormControl(null, [
+        Validators.required,
+        this.forbiddenNames.bind(this),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email])
+      // async doesnt work
+      // asyncValidators: [this.forbiddenEmails.bind(this)],
+      // updateOn: 'blur', 
     });
+
+    this.signupForm.valueChanges.subscribe(val=>console.log(val));
+    this.signupForm.statusChanges.subscribe(val=>console.log(val));
+    this.signupForm.setValue({'userName':"toli","email":"aaa@aa.com"});
+    
   }
 
-  onSubmit()
-  {
+  onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
-  forbiddenNames(control:FormControl): {[key:string]:boolean} | null
-  {
-    if(this.userNamesInvalid.indexOf(control.value)>=0)
-    {
-        return {"xxx":true};
+  forbiddenNames(control: FormControl): { [key: string]: boolean } | null {
+    if (this.userNamesInvalid.indexOf(control.value) >= 0) {
+      return { xxx: true };
     }
 
     return null;
   }
+
+  
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "xxx@xxx.com") {
+          resolve({ 'emailxxx': true });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
+  
+    return promise;
+  }
+  
 }
