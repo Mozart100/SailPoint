30db2f9ad48e9175180ebb7733306161e314290a
Arkovean <anatoliy.arkove@gmail.com>
2024-03-03 20:09:53 +0200

    working

--- SailPoint.Client/sailpoint/src/app/display-all-cities/display-all-cities.component.ts
+++ SailPoint.Client/sailpoint/src/app/display-all-cities/display-all-cities.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CityLocatorService } from '../services/city.locator.service';
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-display-all-cities',
@@ -12,47 +12,37 @@ export class DisplayAllCitiesComponent implements OnInit {
   cities$: Observable<string[]> = new Observable<string[]>();
 
   col: number = 4;
-  rowHeight  = "3rem";
+  rowHeight = '3rem';
 
   constructor(
     private cityLocatorService: CityLocatorService,
     private breakpointObserver: BreakpointObserver
   ) {
     this.breakpointObserver
-      .observe([
-        Breakpoints.Small,
-        Breakpoints.Medium,
-        Breakpoints.Large
-      ])
-      .subscribe(result => {
+      .observe([Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+      .subscribe((result) => {
         const breakpoints = result.breakpoints;
 
-          if(breakpoints[Breakpoints.Large])
-          {
-            this.col = 10
-            this.rowHeight = "2rem";
+        if (breakpoints[Breakpoints.Large]) {
+          this.col = 10;
+          this.rowHeight = '2rem';
+        } else {
+          if (breakpoints[Breakpoints.Medium]) {
+            this.col = 7;
+            this.rowHeight = '2.5rem';
+          } else {
+            this.col = 4;
+            this.rowHeight = '3rem';
           }
-          else
-          {
-            if(breakpoints[Breakpoints.Medium])
-            {
-              this.col = 7
-              this.rowHeight = "2.5rem";
-
-            }
-            else
-            {
-              this.col = 4;
-              this.rowHeight = "3rem";
-            }
-          }
+        }
       });
   }
 
   ngOnInit(): void {
-    this.cities$ = this.cityLocatorService
-      .getAllCities()
-      .pipe(map((response) => response.cities));
+    this.cities$ = this.cityLocatorService.getAllCities().pipe(
+      tap((res) => console.log(res.cities)),
+      map((response) => response.cities)
+    );
   }
 
   getColor(city: string): string {
